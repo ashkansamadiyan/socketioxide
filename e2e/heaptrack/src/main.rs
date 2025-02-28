ebb9bc8ec964f7bb34268612167f834b9b0ea4e3
ashkansamadiyan <158470661+ashkansamadiyan@users.noreply.github.com>
2025-02-28 11:42:52 +0330

    Update main.rs

--- e2e/heaptrack/src/main.rs
+++ e2e/heaptrack/src/main.rs
@@ -3,7 +3,6 @@ use hyper_util::rt::TokioIo;
 use socketioxide::{extract::SocketRef, SocketIo};
 use std::{net::SocketAddr, time::Duration};
 use tokio::net::TcpListener;
-
 fn on_connect(socket: SocketRef) {
     socket.on("ping", |s: SocketRef| {
         s.emit("pong", &()).ok();
@@ -24,13 +23,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         tokio::time::sleep(Duration::from_secs(60)).await;
         std::process::exit(0);
     });
-
     loop {
         let (stream, _) = listener.accept().await?;
 
         let io = TokioIo::new(stream);
         let svc = svc.clone();
-
         tokio::task::spawn(async move {
             http1::Builder::new()
                 .serve_connection(io, svc)
